e961e510b12a7e972df3c8ea3809aaf317b8edf5
Hemant Makhija <makhijahemant007@gmail.com>
2022-12-23 19:28:19 +0530

    [UPDATE] - Added searched city in kafka published via search api. - Shown recommendations at homepage based on searches fetched via recommendation api.

--- src/Components/PlacesList.js
+++ src/Components/PlacesList.js
@@ -4,16 +4,21 @@ import Places from "./Places";
 import React from "react";
 import TextField from "@mui/material/TextField";
 import { CircularProgress } from "@mui/material";
+import CancelRoundedIcon from "@material-ui/icons/CancelRounded";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 const PlacesList = () => {
-  const baseUrl = "http://localhost:8080/toVisit/place/list";
+  const fetchPlacesUrl = "http://localhost:8080/toVisit/place/list";
+  const searchPlacesUrl = "http://localhost:8080/toVisit/place/search";
 
   const [places, setPlacesList] = useState([]);
 
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(baseUrl).then((response) => {
+    axios.get(fetchPlacesUrl).then((response) => {
       setIsLoading(false);
       setPlacesList(response.data);
       // console.log(response.data);
@@ -22,7 +27,7 @@ const PlacesList = () => {
   }, []);
 
   const updateList = () => {
-    axios.get(baseUrl).then((response) => {
+    axios.get(fetchPlacesUrl).then((response) => {
       setPlacesList(response.data);
     });
   };
@@ -35,11 +40,30 @@ const PlacesList = () => {
       : true
   );
 
+  //to ensure api is called after 1.5 sec delay of the last keystroke
+  const [timer, setTimer] = useState(null);
+
   const OnChangeHandler = (event) => {
     var city = event.target.value.toLowerCase();
     setSearchCity(city);
     // console.log("searched " + city);
     // console.log("filteredPlacesList length " + filteredPlacesList.length);
+
+    clearTimeout(timer);
+
+    const newTimer = setTimeout(() => {
+      if (filteredPlacesList.length === 1) {
+        axios.get(searchPlacesUrl, {
+          params: { place: filteredPlacesList[0].city },
+        });
+      }
+    }, 1500);
+
+    setTimer(newTimer);
+  };
+
+  const handleClear = () => {
+    setSearchCity("");
   };
 
   return (
@@ -52,6 +76,20 @@ const PlacesList = () => {
           variant="outlined"
           placeholder="Search by city"
           onChange={OnChangeHandler}
+          value={searchCity}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+
+            endAdornment: searchCity && (
+              <IconButton onClick={handleClear}>
+                <CancelRoundedIcon />
+              </IconButton>
+            ),
+          }}
         />
       </div>
 
--- src/Components/Recommendations.js
+++ src/Components/Recommendations.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Recommendation from "./Recommendation";
+import Typography from "@mui/material/Typography";
+import { CircularProgress } from "@mui/material";
 
 const Recommendations = () => {
-  return <div>Reco</div>;
+  const fetchRecommendationsUrl =
+    "http://localhost:8080/toVisit/place/recommendations";
+
+  const [recommendations, setRecommendations] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(fetchRecommendationsUrl).then((response) => {
+      console.log(" response " + JSON.stringify(response.data));
+      console.log(" length " + response.data.length);
+      setIsLoading(false);
+      setRecommendations(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <div style={{ margin: 10 }}>
+        <Typography style={{ marginBottom: 20 }}>Recommended Places</Typography>
+
+        {isLoading ? (
+          <CircularProgress color="inherit" />
+        ) : (
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {recommendations.length > 0
+              ? recommendations.map((recommendation) => (
+                  <Recommendation
+                    key={recommendation.id}
+                    city={recommendation.place.city}
+                    attraction={recommendation.place.attraction}
+                    month={recommendation.place.month}
+                  />
+                ))
+              : "No recommendations found"}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Recommendations;
